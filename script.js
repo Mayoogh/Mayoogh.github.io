c7b30e668fd631fa8b8d3bcc39cc64f097416768
Mayoogh Girish <mayoogh1997@gmail.com>
2018-09-10 21:40:42 +0530

    Smart_Ride_beta
    Firebase testing

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyAQ95kXUwc0xpGncQFYP-_ZdOH0SjwxJKM",
-    authDomain: "led-control-85a90.firebaseapp.com",
-    databaseURL: "https://led-control-85a90.firebaseio.com",
-    projectId: "led-control-85a90",
-    storageBucket: "led-control-85a90.appspot.com",
-    messagingSenderId: "753326420890"
+    apiKey: "AIzaSyDff13CZ58yiyG9jpZDzDjlvNgMhCRLgDs",
+    authDomain: "smartride-8fd1f.firebaseapp.com",
+    databaseURL: "https://smartride-8fd1f.firebaseio.com",
+    projectId: "smartride-8fd1f",
+    storageBucket: "smartride-8fd1f.appspot.com",
+    messagingSenderId: "12233100788"
   };
   firebase.initializeApp(config);
 
 $(document).ready(function(){
   var database = firebase.database();
-  var ledStatus;
+  var EngineStatus;
+  var BatteryStatus;
 
   database.ref().on("value", function(snap){
-    ledStatus = snap.val().ledStatus;
-    if(ledStatus == 1){
+    EngineStatus = snap.val().EngineStatus;
+    EngineStatus = snap.val().EngineStatus;
+    if(EngineStatus == 1){
       // $(".lightStatus").text("The light is on");
     } else {
       // $(".lightStatus").text("The light is off");
@@ -23,14 +25,22 @@ $(document).ready(function(){
   });
 
   $(".lightButton").click(function(){
-    var firebaseRef = firebase.database().ref().child("ledStatus");
+    var firebaseRef = firebase.database().ref().child("EngineStatus");
 
-    if(ledStatus == 1){
+    if(EngineStatus == 1){
       firebaseRef.set(0);
-      ledStatus = 0;
+      EngineStatus = 0;
     } else {
       firebaseRef.set(1);
-      ledStatus = 1;
+      EngineStatus = 1;
+    }
+    
+    if(BatteryStatus == 1){
+      firebaseRef.set(0);
+      BatteryStatus = 0;
+    } else {
+      firebaseRef.set(1);
+      BatteryStatus = 1;
     }
   });
 });
